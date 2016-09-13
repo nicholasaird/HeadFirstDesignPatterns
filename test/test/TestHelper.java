32ce946b7f75b94091f60ba3571b73da82ca0941
Nicholas Aird <nicholas.aird@videotron.ca>
2016-09-13 17:22:52 -0400

    New method assertNotEqual()

---
+++ test/test/TestHelper.java
@@ -0,0 +1,14 @@
+package test;
+
+import static org.junit.Assert.assertTrue;
+
+public final class TestHelper {
+    private TestHelper() {
+        // Utility classes should not have public constructors
+    }
+
+    public static void assertNotEquals(Object expected, Object actual) {
+        assertTrue("expected <" + actual + "> to be different from <" + expected
+                + ">", !expected.equals(actual));
+    }
+}
